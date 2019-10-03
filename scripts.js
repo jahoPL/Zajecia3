16a48a6f293890dc681c1b05e0cbe575dab35573
jahoPL <38810823+jahoPL@users.noreply.github.com>
2019-10-03 19:54:55 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,56 @@
 console.log("skrypty załadowane!")
+
+
+const owce = [{
+    name: "asas",
+    wiek: 8,
+    kolor:"biały"
+},
+{
+    name: "Andrzej",
+    wiek: 16,
+    kolor:"biały"
+},
+{
+    name: "Jędrzej",
+    wiek: 14,
+    kolor:"czarny"
+},
+{
+    name: "sasa",
+    wiek: 8,
+    kolor:"biały"
+},
+{
+    name: "mruczek",
+    wiek: 10,
+    kolor:"biały"
+},
+{
+    name: "chau-chau",
+    wiek: 6,
+    kolor:"biały"
+},
+{
+    name: "chau-chau",
+    wiek: 6,
+    kolor:"biały"
+},
+{
+    name: "ciapka",
+    wiek: 12,
+    kolor:"Dalmatyńczyk"
+},
+]
+
+const reprezentacjaOwiec = owce.map( owca => `
+    <div class="owca">
+    ${owca.imie}
+    ${owca.kolor}
+    ${owca.wiek}
+    <div/>
+    `)
+
+const tekstDoWstawienia = reprezentacjaOwiec.join("");
+const miejsceNaTekst = document.querySelector("#miejsceNaOwce")
+miejsceNaTekst.innerHTML = tekstDoWstawienia; 
